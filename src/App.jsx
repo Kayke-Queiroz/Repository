20220e2490bb8fea6730769198ff6a36a743986c
Kayke Queiroz dos Santos <kaykekato@gmail.com>
2026-02-09 10:33:23 -0300

    Disable loading screen in App for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import useLoading from './hooks/useLoading'
-import Loading from './components/Loading'
-
 export default function App() {
-  const isLoading = useLoading(2000) // tempo de exibição do loading 
-
   return (
-    <>
-      {isLoading && <Loading />}
-      {!isLoading && (
-        <div className="min-h-screen bg-black text-white flex items-center justify-center">
-          Conteúdo principal
-        </div>
-      )}
-    </>
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      Conteúdo principal
+    </div>
   )
 }
